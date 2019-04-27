105912a308e161ab2d43f415c1beaca7514b7a99
yhl234 <200392342@student.georgianc.on.ca>
2019-04-27 14:39:31 -0400

    cathaybk

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const currency = function(){
+	request({
+		url: "https://www.cathaybk.com.tw/cathaybk/personal/exchange/product/currency-billboard/?indexwidget",
+		method: "GET"
+	}, function(error, response, body){
+		if(error || !body){
+			return;
+		}else{
+			const $ = cheerio.load(body);
+			const result = [];
+			const table_tr = $("#content-first-tab-01 table tbody tr");
+			for(var i=0; i<table_tr.length;i++){
+				const table_td = table_tr.eq(i).find('td');
+				const country = table_td.eq(0).text().trim();
+				const bankBuy = table_td.eq(1).text(); 
+				const bankSell = table_td.eq(2).text(); 
+				result.push(Object.assign({country,bankBuy,bankSell}))
+			}
+			console.log(result);
+		}
+	})
+}
+currency();
